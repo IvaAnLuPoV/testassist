3680f8a451a076be91ea4dde13120d4c149604b9
Ivan Lupov <ivan.lupov04@gmail.com>
2026-02-06 15:12:43 +0200

    Removed rudimentary check in run_gen.py protecting from paralelism recursion.

--- demo/run_gen.py
+++ demo/run_gen.py
@@ -1,26 +1,22 @@
 from testassist import TestAssist, Choice, Range
 import os
 
-def main():
-    ta = TestAssist(seed=42)
+ta = TestAssist(seed=42)
 
-    ta.begin_subtask()
-    ta.make_batch(6, {
-        'testgen': 'demo/testgens/py_gen.py',
-        'n': Choice([5, 10]),
-        'm': Choice([2, 6, 12])
-    })
+ta.begin_subtask()
+ta.make_batch(6, {
+    'testgen': 'demo/testgens/py_gen.py',
+    'n': Choice([5, 10]),
+    'm': Choice([2, 6, 12])
+})
 
-    ta.begin_subtask()
-    ta.make_batch(5, {
-        'testgen' : 'demo/testgens/cpp_gen',
-        'n': Range(5, 10),
-        'm': Range(6, 8)
-    })
+ta.begin_subtask()
+ta.make_batch(5, {
+    'testgen' : 'demo/testgens/cpp_gen',
+    'n': Range(5, 10),
+    'm': Range(6, 8)
+})
 
-    ta.preview()
+ta.preview()
 
-    ta.finalize(workers=4)
-
-if __name__ == "__main__":
-    main()
+ta.finalize(workers=4)
